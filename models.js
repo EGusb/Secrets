794b2d580772a06d886f19f2595f071b80395951
Emiliano Gusberti <emggw@hotmail.com>
2023-03-10 21:35:47 -0300

    Add encryption to userSchema password

--- models.js
+++ models.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 mongoose.set("strictQuery", false);
 
 const dbFullPath = `${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const secret = process.env.SECRET_STR;
 console.log(`Database path: ${dbFullPath}`);
 mongoose.connect(dbFullPath);
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -15,6 +17,7 @@ const userSchema = {
     type: String,
     required: true,
   },
-};
+});
 
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 exports.User = mongoose.model("User", userSchema);
